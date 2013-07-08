1e6531840ce53675029b190b5a7fe6025a3ced52
Gene Le <gene.gkl@gmail.com>
2013-07-08 11:57:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readit::Application.config.secret_token = 'e4b47c72dc0f9f0f60ec8f4084cefe76aaecaaec40cb23f3bdde191f0fd5d13b586715a230f7ab17e549deaadc389c5533665c3d7aca612d7cb8a563bc2cc77b'
